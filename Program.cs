1ac25577baf97c354abafc4c7c138331cbbb2b27
Stanislas d'Andlau <24450@student.dorset-college.ie>
2021-10-06 17:24:03 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using oop_lesson4_parking.test;
+
+namespace oop_lesson4_parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Driver driver = new Driver();
+            driver.CreateObjects();
+        }
+    }
+}
---
+++ models/CarParkCharge.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oop_lesson4_parking.models
+{
+    public class CarParkCharge
+    {
+        public int minimumFee { get; set; }
+        public int minimumHours { get; set; }
+        public CarParkCharge(int _minimumFee = 2, int _minimumHours=3)
+        {
+            Console.WriteLine("I am the CarParkCharge()");
+            minimumFee = _minimumFee;
+            minimumHours = _minimumHours;
+        }
+
+        public override string ToString()
+        {
+            return "I am the CarParkCharge()";
+        }
+
+        public double CalculateCharge(int hoursParked)
+        {
+            
+            double calculatedFee = hoursParked <= minimumHours ? minimumFee : minimumFee + (hoursParked-3)*0.5;
+            return calculatedFee;
+        }
+    }
+}
